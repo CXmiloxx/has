274b050d074450dd319562c7db60de516d5ddc5e
Camilo <juancamilog9911@gmail.com>
2025-03-25 19:58:39 -0500

    ♻️ Refactor: mejora diseño de componente
    se mejora el diseño añadiendo mas animaciones y mejorando la paleta de colores

--- src/components/ButtonTheme.jsx
+++ src/components/ButtonTheme.jsx
@@ -1,21 +1,19 @@
-// eslint-disable-next-line no-unused-vars
-import { motion} from 'framer-motion';
-import { FaSun, FaMoon} from 'react-icons/fa';
-
-
+/* eslint-disable no-unused-vars */
+import { motion } from 'framer-motion';
+import { FaSun, FaMoon } from 'react-icons/fa';
 import { useTheme } from '#/ThemeContext';
 
 export default function ButtonTheme() {
   const { theme, toggleTheme } = useTheme();
+
   return (
-    <>
-      <motion.button
-        whileTap={{ scale: 0.9 }}
-        onClick={toggleTheme}
-        className="p-2 rounded-lg bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white"
-      >
-        {theme === 'dark' ? <FaSun size={20} /> : <FaMoon size={20} />}
-      </motion.button>
-    </>
+    <motion.button
+      whileTap={{ scale: 0.85 }}
+      whileHover={{ scale: 1.1, rotate: 10 }}
+      onClick={toggleTheme}
+      className="p-3 rounded-full shadow-lg bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white transition-all duration-300"
+    >
+      {theme === 'dark' ? <FaSun size={24} className="text-yellow-500" /> : <FaMoon size={24} className="text-blue-500" />}
+    </motion.button>
   );
 }
